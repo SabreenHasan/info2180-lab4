da4dc1a0bc5098ce06dfe831b89b708d11dedca7
Sabreen Hasan <sabreen_hasan@yahoo.com>
2019-10-20 08:58:57 -0700

    Exercise one complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,16 @@
 window.onload = function() {
 
-	
+	var body = document.getElementsByTagName("body");
+	console.log(body);
+
+	var BOARD_SIZE = 9;
+	var GRID_SIZE = 12;
+	var board = document.getElementById("board"); 
+
+
+	for (var i = 3; i < GRID_SIZE; i++) { //Iterates through all the divs but begins at the 3rd grid that makes the board.
+		var squares = document.getElementsByTagName("div")[i].setAttribute("class", "square");
+		console.log(squares);
+	}
 }
+	
