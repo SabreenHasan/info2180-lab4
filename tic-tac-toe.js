a6a633a8b2fc522723ac58bf0ae4dd08b001ac86
Sabreen Hasan <sabreen_hasan@yahoo.com>
2019-10-21 02:20:08 -0700

    Exercise two complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,16 +1,33 @@
 window.onload = function() {
-
-	var body = document.getElementsByTagName("body");
-	console.log(body);
-
-	var BOARD_SIZE = 9;
-	var GRID_SIZE = 12;
-	var board = document.getElementById("board"); 
-
-
-	for (var i = 3; i < GRID_SIZE; i++) { //Iterates through all the divs but begins at the 3rd grid that makes the board.
-		var squares = document.getElementsByTagName("div")[i].setAttribute("class", "square");
-		console.log(squares);
-	}
+	startGame()
+	document.getElementsByClassName("btn")[0].onclick = function(){
+        for(let i = 0; i < squares.length; i++){
+            squares[i].innerHTML = ("")
 }
+
+	function startGame() {
+		var GRID_SIZE = 12;
+		var squares = document.getElementsByTagName("div");
+		for (var i = 3; i < GRID_SIZE; i++) { //Iterates through all the divs but begins at the 3rd grid that makes the board.
+			squares[i].setAttribute("class", "square");
+			squares[k].onmouseover = classList.add("hover");
+			squares[k].onmouseout = classList.add("square");
+			squares[i].onclick = function() {  //Checks if someone clicks a square on the board
+			if (squares[i].innerHTML === "" || squares[i].innerHTML === "O") {
+				squares[i].classList.toggle("X", true)
+				squares[i].innerHTML = "X"; 
+				squares[i].classList.toggle("O", false)
+			}
+
+			else if (squares[i].innerHTML === "X") {
+				squares[i].classList.toggle("X", false)
+				squares[i].innerHTML = "O"
+				squares[i].classList.toggle("O", true)
+			}
+			
+		}	
+		console.log(squares);	
+	}
+		
+
 	
